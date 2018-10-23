8202f197f66110b24287642cd0d160c9549695a6
Mike Morain <mike.morain+git@gmail.com>
2018-10-23 16:33:56 +0100

    Fixing google search results with new header and better parsing.

--- Wox.Plugin.GoogleSearch/GoogleSearch.cs
+++ Wox.Plugin.GoogleSearch/GoogleSearch.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Web;
+using HtmlAgilityPack;
 
 namespace Wox.Plugin.GoogleSearch
 {
     public class GoogleSearch
     {
         // Inspired heavily by: https://github.com/aviaryan/alfred-google-search/blob/master/src/gsearch/googlesearch.py
-        private const string UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 " + 
-                                         "(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+        private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                                         "(KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
 
         private readonly HttpClient _client;
 
@@ -27,11 +28,38 @@ namespace Wox.Plugin.GoogleSearch
         {
             query = query.Replace(' ', '+');
             var response = _client.GetAsync(BuildSearchUri(query));
-            var results = ParseResponse(response.Result).ToList();
+            var results = ParseResponseWithHAP(response.Result).ToList();
             
             return results.Take(limit);
         }
 
+        private static IEnumerable<GoogleSearchResult> ParseResponseWithHAP(HttpResponseMessage response)
+        {
+            var htmlDoc = new HtmlDocument();
+            var googleSearchResults = new List<GoogleSearchResult>();
+            
+            htmlDoc.LoadHtml(response.Content.ReadAsStringAsync().Result);
+
+            var allElementsWithClassR = htmlDoc.QuerySelectorAll("div.r");
+
+            
+            foreach (var e in allElementsWithClassR)
+            {
+                var link = e.QuerySelector("a").Attributes.First(a => a.Name == "href").Value;
+                var title = e.QuerySelector("h3").InnerText;
+                Console.WriteLine("Title: " + title);
+                Console.WriteLine("Link: " + link);
+
+                googleSearchResults.Add(new GoogleSearchResult()
+                {
+                    Name = title,
+                    Url = link
+                });
+            }
+
+            return googleSearchResults;
+        }
+        
         private static IEnumerable<GoogleSearchResult> ParseResponse(HttpResponseMessage response)
         {
             var headerRegex = new Regex("<h3.*?>.*?</h3>", RegexOptions.IgnoreCase);
